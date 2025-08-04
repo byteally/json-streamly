6ceb5b1b3fc12d46755bb78c7aecaa80b748f098
Sreenidhi <nair.sreenidhi@gmail.com>
2025-08-05 05:25:23 +0530

    Fix parser edge case

--- src/Streamly/Json.hs
+++ src/Streamly/Json.hs
@@ -1,13 +1,16 @@
+{-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase       #-}
 module Streamly.Json
   ( decodeFile
   , decodeFileWithBufferOf
+  , decodeStream
   ) where
 
 import           Control.Monad.Catch
-import           Data.Either
 import           Data.JsonStream.Parser
+import           Data.Text ( Text )
+import qualified Data.Text as T
 import           Data.Word
 import           Streamly.Data.Array
 import qualified Streamly.Data.Stream.Prelude as Stream
@@ -17,31 +20,24 @@ import qualified Streamly.Internal.Data.Stream as Stream
 import qualified Streamly.Internal.FileSystem.File as File
 
 -- Uses `defaultChunkSize`
-decodeFile :: (MonadCatch m, Stream.MonadAsync m) => FilePath -> Parser a -> Stream.Stream m a
+decodeFile :: (MonadCatch m, Stream.MonadAsync m, Show a) => FilePath -> Parser a -> Stream.Stream m (Either Text a)
 decodeFile fp p = flip decodeStream p $ File.readChunks fp
 
-decodeFileWithBufferOf :: (MonadCatch m, Stream.MonadAsync m) => Int -> FilePath -> Parser a -> Stream.Stream m a
+decodeFileWithBufferOf :: (MonadCatch m, Stream.MonadAsync m, Show a) => Int -> FilePath -> Parser a -> Stream.Stream m (Either Text a)
 decodeFileWithBufferOf sz fp p = flip decodeStream p $ File.readChunksWith sz fp
 
--- TODO: handle ParseFailed
-decodeStream :: (MonadCatch m, Stream.MonadAsync m) => Stream.Stream m (Array Word8) -> Parser a -> Stream.Stream m a
+decodeStream :: (MonadCatch m, Stream.MonadAsync m, Show a) => Stream.Stream m (Array Word8) -> Parser a -> Stream.Stream m (Either Text a)
 decodeStream w8ArrStrm p = Stream.unfoldMany Unfold.fromList
-                           $ Stream.mapMaybe (\case
-                                          (_, Right mv) -> mv
-                                          _ -> error "Panic: Unreachable pat Left"
-                                          )
-                           $ Stream.takeWhile (isRight . snd)
-                           $ Stream.scanl' (\(po, _) w8Arr -> case po of
-                                        ParseNeedData k -> (k $ Strict.fromArray w8Arr, Right Nothing)
-                                        ParseFailed err -> (po, Left $ Just err)
-                                        ParseDone _rst -> (po, Left Nothing)
-                                        po'@ParseYield {} ->
-                                          fmap (Right . Just) $ accParsedValues w8Arr po'
-                                    ) (runParser p, Right Nothing) w8ArrStrm
-  where    
-    accParsedValues :: Array Word8 -> ParseOutput a -> (ParseOutput a, [a])
-    accParsedValues w8Arr po = go po []
-      where
-        go (ParseYield v po') acc = go po' (v : acc)
-        go (ParseNeedData k) acc = (k $ Strict.fromArray w8Arr, Prelude.reverse acc)
-        go po' acc = (po', Prelude.reverse acc)
+                           $ Stream.map snd
+                           $ Stream.postscanl' (\(po, _) w8Arr -> case po of
+                                        ParseNeedData k ->
+                                          case k $ Strict.fromArray w8Arr of
+                                            po'@ParseYield {} -> consume po' []
+                                            _ -> (k $ Strict.fromArray w8Arr, [])
+                                        ParseFailed err -> (po, [Left $ T.pack err])
+                                        po'@ParseDone {} -> error $ "Panic: Unexpected ParseDone @decodeStream: " <> show po'
+                                        po'@ParseYield {} -> error $ "Panic: Unexpected ParseYield @decodeStream: " <> show po'
+                                    ) (runParser p, []) w8ArrStrm
+  where
+    consume (ParseYield v po') acc = consume po' (Right v : acc)
+    consume k acc = (k, reverse acc)
